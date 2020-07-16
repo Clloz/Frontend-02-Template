99790e15b49e1d77aff49b3e4a856a78103dd7ce
clloz <clloz@outlook.com>
2020-07-16 16:01:47 +0800

    week02

--- week02/utf8_encoding.js
+++ week02/utf8_encoding.js
@@ -1,37 +1,44 @@
-function utf8_encoding(str) {
-    const length = str.length;
-    let arr = [];
-    for (let i = 0; i < length; i++) {
-        let cp = str.codePointAt(i);
-        let b = cp.toString(2);
-        let res = '';
-        switch (true) {
-            case (cp <= 127):
-                res = b;
-                break;
-            case (cp <= 2047):
-                b = b.length < 11 ? b.addZero(11 - b.length) : b;
-                res = `110${b.slice(0, 5)}10${b.slice(5)}`;
-                arr.push(res);
-                break;
-            case (cp <= 65535):
-                b = b.length < 16 ? b.addZero(16 - b.length) : b;
-                res = `1110${b.slice(0, 4)}10${b.slice(4, 10)}10${b.slice(10)}`;
-                break;
-            case (cp > 65535):
-                b = b.length < 21 ? b.addZero(21 - b.length) : b;
-                res = `11110${b.slice(0, 3)}10${b.slice(3, 9)}10${b.slice(9, 15)}10${b.slice(15)}`;
-                break;
-        }
-        arr.push('0x' + parseInt(res, 2).toString(16).toUpperCase());
-    }
-    return arr.join();
+function utf8_encoding1 (str) {
+	const code = encodeURIComponent(str);
+	const byte = [];
+	for (let i = 0; i < code.length; i++) {
+		const c = code.charAt(i);
+		if (c === '%') {
+			const hex = code.charAt(i + 1) + code.charAt(i + 2);
+			const hexVal = parseInt(hex, 16);
+			byte.push(hexVal);
+			i += 2;
+		} else byte.push(c.charCodeAt(0))
+	}
+	return '0x' + byte.map(c => c.toString(16)).join('');
+}
+console.log(utf8_encoding1('𝌆')) //0xf09d8c86
+
+
+function utf8_encoding2 (str) {
+	const length = str.length;
+	let arr = [];
+	for (let i = 0; i < length; i++) {
+		let cp = str.codePointAt(i);
+		let result = '0x';
+		if (cp <= 0x7F) {
+			result += cp & 0x7F
+		} else if (cp >= 0x80 && cp <= 0x7FF) {
+			result += (cp >> 6 & 0x1F | 0xC0).toString(16)
+			result += (cp & 0x3F | 0x80).toString(16)
+		} else if (cp >= 0x800 && cp <= 0xFFFF) {
+			result += (cp >> 12 & 0xF | 0xE0).toString(16)
+			result += (cp >> 6 & 0x3F | 0x80).toString(16)
+			result += (cp & 0x3F | 0x80).toString(16)
+		} else if (cp >= 0x10000 && cp <= 0x10FFFF) {
+			result += (cp >> 18 & 0x7 | 0xF0).toString(16)
+			result += (cp >> 12 & 0x3F | 0x80).toString(16)
+			result += (cp >> 6 & 0x3F | 0x80).toString(16)
+			result += (cp & 0x3F | 0x80).toString(16)
+		}
+		arr.push(result)
+	}
+	return arr
 }
 
-String.prototype.addZero = function (num) {
-    let str = '';
-    for (let i = 0; i < num; i++) {
-        str += '0';
-    }
-    return str += this;
-}
+console.log(utf8_encoding2('𝌆')) //[ '0xf09d8c86', '0xedbc86' ]
