bb9d6cee9f92975d3cc0537fac635f00ef04221f
clloz <clloz@outlook.com>
2020-08-30 22:15:07 +0800

    week09

---
+++ week09/.eslintrc.js
@@ -0,0 +1,22 @@
+/*
+ * @Author: Clloz
+ * @Date: 2020-08-29 21:53:00
+ * @LastEditTime: 2020-08-29 22:39:17
+ * @LastEditors: Clloz
+ * @Description:
+ * @FilePath: /week09/.eslintrc.js
+ * @博观而约取，厚积而薄发，日拱一卒，日进一寸，学不可以已。
+ */
+module.exports = {
+    env: {
+        browser: true,
+        es2020: true,
+        node: true,
+    },
+    extends: 'eslint:recommended',
+    parserOptions: {
+        ecmaVersion: 11,
+    },
+    plugins: ['html'],
+    rules: {},
+};
